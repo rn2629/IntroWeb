87f6214458a3371f112a6a2dce1e2fc68912fec1
ngaro1830511 <ngaro1830511@W10-INFO-4>
2018-11-01 11:31:08 -0400

    Exercices JS 6

--- ExercicesJS/Exercice11 -  Si composé/JS/traitement11.js
+++ ExercicesJS/Exercice11 -  Si composé/JS/traitement11.js
@@ -1,5 +1,19 @@
 function btnCalculer_onclick()
 { 
- 	
+ 	var Nbre1, Nbre2, Signe;
 
+ 	Nbre1 = parseFloat(document.getElementById("txtNbre1").value);
+ 	Nbre2 = parseFloat(document.getElementById("txtNbre2").value);
+
+ 	if ((Nbre1>= 0 && Nbre2>=0) || (Nbre1<=0 && Nbre2<=0))
+    {
+        Signe = "Positif";
+    }
+    else
+    {
+        Signe = "Negatif";
+    }
+
+    document.getElementById("lblMessage").innerHTML = "Votre Signe est " + Signe;
 }
+
