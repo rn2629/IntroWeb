8aa855aa76d52190901f60950437630296eb6833
ngaro1830511 <ngaro1830511@W10-INFO-4>
2018-11-15 10:24:52 -0500

    Exercices JS Joueurs

--- ExercicesJS/Exercice10 CinéClub - V2/JS/traitement.js
+++ ExercicesJS/Exercice10 CinéClub - V2/JS/traitement.js
@@ -1,6 +1,15 @@
+
+var PrixBase, Age, Montant, Representation, MiSession, Listes, NomFilm;
+
 function btnCalculer_onclick()
 {
-    var PrixBase, Age, Montant, Representation, MiSession, Listes;
+    SaisirVariables();
+    saisirMiSession();
+    calculerMoy();
+}
+
+function SaisirVariables()
+{
 
     Age = parseInt(document.getElementById("txtAge").value);
     Listes = document.getElementById("lstOptions").value;
@@ -8,7 +17,11 @@ function btnCalculer_onclick()
     Representation = document.getElementById("radMidi","radSoir").value;
     Age = parseInt(document.getElementById("txtAge").value);
     PrixBase = parseFloat(document.getElementById("txtBasePrix").value);
+    NomFilm = document.getElementById("Film").value;
+}
 
+function saisirMiSession()
+{
     if (document.getElementById("chkMiSession").checked == true)
     {
 
@@ -40,14 +53,28 @@ function btnCalculer_onclick()
 
 
     }
-   else {
+    else {
         MiSession = 0;
     }
+
+}
+
+function lstOptions_onchange()
+{
+    var Film;
+
+    Film = document.getElementById("lstOptions").value;
+
+    document.getElementById("imgFilm").src = "img/" + Film + ".jpg";
+}
+
+function calculerMoy()
+{
 
     MiSession = PrixBase*MiSession;
     Montant = (PrixBase-MiSession);
 
-    console.log(" Le montant sera de : " + Montant + "$" + " Pour la Representation de  " + Representation + " du film " + Listes + " et spectateurs de " + Age + "ans");
+    document.getElementById("lblMessage").innerHTML = " Le montant sera de : " + Montant + "$" + " Pour la Representation de  " + Representation + " du film " + Listes + " et spectateurs de " + Age + "ans";
 
 
 }
