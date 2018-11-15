1ed7a28b812ad23e66e667d43c1f6d8a75298ae8
ngaro1830511 <ngaro1830511@W10-INFO-4>
2018-11-15 10:57:15 -0500

    Exercices JS Joueurs retournement

--- ExercicesJS/Exercice13 _ Joueurs/JS/traitement.js
+++ ExercicesJS/Exercice13 _ Joueurs/JS/traitement.js
@@ -4,7 +4,11 @@ var NomJoueur, TabPoints = new Array(4),TabJoueur = new Array(4),NbPoints, Cpt =
 
 function btnAjouter_onclick()
 {
+Ajouter();
+}
 
+function Ajouter()
+{
     NomJoueur = document.getElementById("txtNom").value;
     NbPoints = parseFloat(document.getElementById("txtPoints").value);
     TabJoueur [Cpt]= NomJoueur;
@@ -21,25 +25,38 @@ function btnAjouter_onclick()
         document.getElementById("btnRechercher").disabled = false;
     }
 
-
 }
 
 function btnTrouverMoy_onclick()
 {
-   var  Total = 0, Moyenne = 0;
+  res = Moyenne();
+}
+
+function Moyenne()
+{
+    var res;
+    var  Total = 0, Moyenne = 0;
 
     for (var i =0; i<TabPoints.length ; i++)
     {
-       Total += TabPoints[i] ;
+        Total += TabPoints[i] ;
 
     }
 
     Moyenne = Total/4;
-    document.getElementById("lblReponse").innerHTML = "La moyenne est de:" + Moyenne;
+    res = document.getElementById("lblReponse").innerHTML = "La moyenne est de:" + Moyenne;
+
+    return  res;
 }
 
 function btnTrouverMeilleur_onclick()
 {
+    res = MeilleurP();
+}
+
+function MeilleurP()
+{
+    var MeilleurP;
     var MeilleurPoint = TabPoints[0];
     var NomMeilleurJoueur = TabJoueur[0];
 
@@ -52,16 +69,23 @@ function btnTrouverMeilleur_onclick()
         }
     }
 
-    document.getElementById("lblReponse").innerHTML = "Le meilleur Pointage est: " + MeilleurPoint + " et le nom du Joueur est " + NomMeilleurJoueur ;
+   MeilleurP = document.getElementById("lblReponse").innerHTML = "Le meilleur Pointage est: " + MeilleurPoint + " et le nom du Joueur est " + NomMeilleurJoueur ;
+
+    return MeilleurP;
 }
 
 function btnTrouverPire_onclick()
 {
+    PireP()
+}
 
+function PireP()
+{
+    var PireP;
     var PirePoint = TabPoints[0];
     var NomPireJoueur = TabJoueur[0];
 
-     for (var i=0 ; i<TabPoints.length; i++)
+    for (var i=0 ; i<TabPoints.length; i++)
     {
         if (PirePoint>TabPoints[i])
         {
@@ -70,31 +94,17 @@ function btnTrouverPire_onclick()
         }
     }
 
-    document.getElementById("lblReponse").innerHTML = " Le pire Pointage est : " + PirePoint + " et le nom du Joueur est " + NomPireJoueur;
+    PireP = document.getElementById("lblReponse").innerHTML = " Le pire Pointage est : " + PirePoint + " et le nom du Joueur est " + NomPireJoueur;
+
+    return PireP;
 }
 
 function btnRechercher_onclick()
 {
-    cpt  = 0;
+
 
     var rechercher = TabJoueur[cpt];
     var PointRecherche = TabPoints[cpt];
-
-  do {
-      cpt = -1;
-      cpt++;
-
-
-      if (rechercher == TabJoueur[cpt] && rechercher == TabJoueur[cpt] && rechercher == TabJoueur[cpt] && rechercher == TabJoueur[cpt])
-      {
-
-          document.getElementById("lblReponse").innerHTML = " Le nom du joueur que vous rechercher est : " + rechercher + " et le pointage est " + PointRecherche;
-      }
-
-  }while (rechercher != TabJoueur[cpt] && rechercher != TabJoueur[cpt] && rechercher != TabJoueur[cpt] && rechercher != TabJoueur[cpt]) ;
-
-
-
 
 }
 
