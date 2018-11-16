b182d4377cb4e6424d8e6787c319670b4fc9642a
ngaro1830511 <ngaro1830511@W10-INFO-4>
2018-11-16 14:40:57 -0500

    Exercices JS Calculatrice V2

--- ExercicesJS/Exercice12 -  Choisir Parmi/JS/traitement14.js
+++ ExercicesJS/Exercice12 -  Choisir Parmi/JS/traitement14.js
@@ -1,59 +1,13 @@
-function btnCalculer_onclick()
-{
-    if (validChampOblig()=== true)
-    {
-        traiterInfos();
-    }
-}
-
-function traiterInfos()
-{
-    var Nbre1, Nbre2, Operateur, Resultat ;
+function btnCalculer_onclick() {
+    var Nbre1, Nbre2, Operateur, Resultat, Resultat1, Resultat2, Resultat3, Resultat4;
 
     Nbre1 = parseFloat(document.getElementById("txtNbre1").value);
     Nbre2 = parseFloat(document.getElementById("txtNbre2").value);
+
     Operateur = document.getElementById("txtOperateur").value;
-    Resultat = calculer(Nbre1,Nbre2,Operateur);
-
-    document.getElementById("lblMessage").innerHTML = "Le resultat est :" + Resultat;
-}
-
-function validChampOblig()
-{
-    var valid = false;
-
-    if ((validExist("txtNbre1")=== true) && (validExist("txtNbre2")=== true) &&  (validExist("txtOperateur")=== true))
-    {
-        valid = true;
-    }
-
-    return valid;
-}
-
-function validExist(nomID)
-{
-    var valide = true;
-
-    if (document.getElementById(nomID).value=== "")
-    {
-        valide = false;
-        document.getElementById(nomID).style.backgroundColor="red";
-    }
-    else
-    {
-        valide = true;
-        document.getElementById(nomID).style.backgroundColor="white";
-    }
-
-    return valide;
-}
-
-function calculer(Nbre1,Nbre2,Operateur)
-{
-    switch (Operateur)
-    {
+    switch (Operateur) {
         case "+":
-            Resultat =  Nbre1 + Nbre2;
+            Resultat = Nbre1 + Nbre2;
             break;
         case "-":
             Resultat = Nbre1 - Nbre2;
@@ -65,6 +19,7 @@ function calculer(Nbre1,Nbre2,Operateur)
             Resultat = Nbre1 / Nbre2;
             break;
     }
-   return Resultat;
 
+
+    document.getElementById("lblMessage").innerHTML = "Le resultat est :" + Resultat;
 }
--- ExercicesJS/Exercice14_Calculatrice V2/JS/traitement14.js
+++ ExercicesJS/Exercice14_Calculatrice V2/JS/traitement14.js
@@ -1,11 +1,55 @@
 function btnCalculer_onclick()
 {
-    var Nbre1, Nbre2, Operateur, Resultat, Resultat1,Resultat2,Resultat3,Resultat4 ;
+    if (validChampOblig()=== true)
+    {
+        traiterInfos();
+    }
+}
+
+function traiterInfos()
+{
+    var Nbre1, Nbre2, Operateur, Resultat ;
 
     Nbre1 = parseFloat(document.getElementById("txtNbre1").value);
     Nbre2 = parseFloat(document.getElementById("txtNbre2").value);
-
     Operateur = document.getElementById("txtOperateur").value;
+    Resultat = calculer(Nbre1,Nbre2,Operateur);
+
+    document.getElementById("lblMessage").innerHTML = "Le resultat est :" + Resultat;
+}
+
+function validChampOblig()
+{
+    var valid = false;
+
+    if ((validExist("txtNbre1")=== true) && (validExist("txtNbre2")=== true) &&  (validExist("txtOperateur")=== true))
+    {
+        valid = true;
+    }
+
+    return valid;
+}
+
+function validExist(nomID)
+{
+    var valide = true;
+
+    if (document.getElementById(nomID).value=== "")
+    {
+        valide = false;
+        document.getElementById(nomID).style.backgroundColor="red";
+    }
+    else
+    {
+        valide = true;
+        document.getElementById(nomID).style.backgroundColor="white";
+    }
+
+    return valide;
+}
+
+function calculer(Nbre1,Nbre2,Operateur)
+{
     switch (Operateur)
     {
         case "+":
@@ -21,11 +65,6 @@ function btnCalculer_onclick()
             Resultat = Nbre1 / Nbre2;
             break;
     }
-
-
-
-    document.getElementById("lblMessage").innerHTML = "Le resultat est :" + Resultat;
-
- 	
+    return Resultat;
 
 }
