311850e39f24a682a0dd948a65b7d85706af299e
ngaro1830511 <ngaro1830511@W10-INFO-4>
2018-11-16 13:55:47 -0500

    Exercices JS Calculatrice V1

--- ExercicesJS/Exercice12 -  Choisir Parmi/JS/traitement14.js
+++ ExercicesJS/Exercice12 -  Choisir Parmi/JS/traitement14.js
@@ -1,11 +1,19 @@
 function btnCalculer_onclick()
 {
-    var Nbre1, Nbre2, Operateur, Resultat, Resultat1,Resultat2,Resultat3,Resultat4 ;
+    var Nbre1, Nbre2, Operateur, Resultat ;
 
     Nbre1 = parseFloat(document.getElementById("txtNbre1").value);
     Nbre2 = parseFloat(document.getElementById("txtNbre2").value);
-
     Operateur = document.getElementById("txtOperateur").value;
+    Resultat = calculer(Nbre1,Nbre2,Operateur);
+
+    document.getElementById("lblMessage").innerHTML = "Le resultat est :" + Resultat;
+}
+
+function calculer(Nbre1,Nbre2,Operateur)
+{
+
+
     switch (Operateur)
     {
         case "+":
@@ -21,11 +29,6 @@ function btnCalculer_onclick()
             Resultat = Nbre1 / Nbre2;
             break;
     }
-
-
-
-    document.getElementById("lblMessage").innerHTML = "Le resultat est :" + Resultat;
-
- 	
+   return Resultat;
 
 }
