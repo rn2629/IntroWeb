92d874e226336c34f6a4bdcb4f2cb5f462d987eb
ngaro1830511 <ngaro1830511@W10-INFO-4>
2018-11-22 10:47:15 -0500

    Exercices JS Expressions regulieres

---
+++ ExercicesJS/Exercice15_Expressions regulieres/JS/traitement.js
@@ -0,0 +1,34 @@
+function btnValider_onclick()  //mettre la fonction dans un IF tester une fonction à la fois
+{
+   alert();
+}
+
+function validerNAS( Chaine )  // numéro d'assurance social 418 789 123
+{
+
+}
+
+function validerTel( Chaine )  // au format (418) 876-8373  un numéro ça change
+{
+
+}
+
+function validerCar( Chaine ) // des chaînes de caractères alphabétiques majuscule et minuscule et accepte le trait d'union pas obligatoire
+{
+
+}
+
+function validerNo( Chaine )   // un chiffre peut etre 0 5 25 200 etc..
+{
+
+}
+
+function verifierEntreDixTrente( Chaine ) //un chiffre entre 10 et 30
+{
+
+}
+
+function verifierEntreUnVingt( Chaine ) //un chiffre entre 1 et 20
+{
+
+}
--- ExercicesJS/Exercice8 GarderieAnimaux/JS/traitement.js
+++ ExercicesJS/Exercice8 GarderieAnimaux/JS/traitement.js
@@ -125,6 +125,36 @@ function radChat_onclick()
     document.getElementById("imgChien-Chat").src = "img/chat.jpg";
 }
 
+function validChamp(ID)
+{
+    var valid = true;
+
+    if(document.getElementById(ID).value === "")
+{
+ valid = false;
+ document.getElementById(ID).style.backgroundColor = "Red";
+}
+else {
+        valid = true;
+        document.getElementById(ID).style.backgroundColor = "White";
+}
+return valid;
+
+}
+
+function validChampOblig()
+{
+    var valide = true;
+    var TabValeurs = new Array("txtNbreJours", "txtNomClient", "txtNumClient")
+
+    for(var i=0 ; i<TabValeurs.length; i++)
+    {
+        valide = false;
+    }
+    return valide;
+}
+
+
 
 function btnCalculer_onclick()
 {
