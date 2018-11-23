6563c3368360400c546c7956a4d3525e4af9e683
ngaro1830511 <ngaro1830511@W10-INFO-4>
2018-11-23 15:08:00 -0500

    Exercices JS LabInstitut

--- ExercicesJS/Exercice_LabInstitut/JS/traitement.js
+++ ExercicesJS/Exercice_LabInstitut/JS/traitement.js
@@ -1,5 +1,53 @@
-function frmMembre_onsubmit()
+function frmMembre_onclick()
 {
-
+    if(validChampOblig()=== true)
+    {
+        SaisirTarifs();
+    }
 
 }
+
+function SaisirTarifs()
+{
+    var Type, Montant;
+    Type = document.getElementById("type").value;
+
+    switch (Type)
+    {
+        case "adulte":
+            Montant = 90;
+            break;
+        case "étudiant":
+            Montant = 60;
+            break;
+        case "retraité":
+            Montant = 80;
+            break;
+    }
+    document.getElementById("resultat").innerHTML ="Le Montant pour un " + Type + " est " + Montant + "$";
+}
+
+function validChampOblig()
+{
+    var Valid = true;
+
+    if(document.getElementById("txtNom").value==="")
+    {
+        Valid = false;
+        document.getElementById("txtNom").style.borderColor="Red";
+    }
+    else
+    {
+        Valid = true;
+        document.getElementById("txtNom").style.borderColor="White";
+    }
+
+
+    return Valid;
+}
+
+function validExist()
+{
+
+}
+
